9cd65492d74c2a7682c6847b48604265ed2620bc
gamename <tennis.n.smith@gmail.com>
2024-08-01 16:51:40 -0500

    componentize everything

--- main/time_sync.h
+++
@@ -1,9 +0,0 @@
-// time_sync.h
-
-#ifndef TIME_SYNC_H
-#define TIME_SYNC_H
-
-// Function to initialize and synchronize system time with NTP server
-void synchronize_time(void);
-
-#endif // TIME_SYNC_H
